e577f010fea42c016ef74ed3285fa36d159e7ea3
Vladyslav <>
2018-11-21 17:52:06 +0200

    no message

---
+++ TestAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAPP
+//
+//  Created by Andrii Mazepa on 11/21/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
