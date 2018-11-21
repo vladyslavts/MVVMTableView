fbc64f112d1453281670af019ec26248a03a781e
Vladyslav <>
2018-11-21 19:03:11 +0200

    Added tableView, added extensions

--- TestAPP/ViewController.swift
+++ TestAPP/ViewController.swift
@@ -18,3 +18,17 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1000
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
